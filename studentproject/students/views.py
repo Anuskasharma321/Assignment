52453fc32a4904e31986b6fbf5a7f9e51d4fd3a6
AnuskaSharma321 <anuskasharmabhattarai@gmail.com>
2026-01-04 14:20:55 +0545

    first commit

---
+++ studentproject/students/views.py
@@ -0,0 +1,34 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+
+# HOME / READ
+def home(request):
+    students = Student.objects.all()
+    return render(request, 'home.html', {'students': students})
+
+# CREATE
+def add_student(request):
+    form = StudentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'form.html', {'form': form})
+
+# UPDATE
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    form = StudentForm(request.POST or None, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'form.html', {'form': form})
+
+# DELETE
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('home')
+    return render(request, 'delete.html', {'student': student})
